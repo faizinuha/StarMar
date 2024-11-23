05b3e01255f03b4092732b459469a1904e01d77c
xavier <167852917+ananta079@users.noreply.github.com>
2024-11-23 16:51:50 +0700

    setup template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,104 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/app-invoice.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 01:59:15 GMT -->
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+<head>
+    <!-- --------------------------------------------------- -->
+    <!-- Title -->
+    <!-- --------------------------------------------------- -->
+    <title>Mordenize</title>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- --------------------------------------------------- -->
+    <!-- Required Meta Tag -->
+    <!-- --------------------------------------------------- -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="handheldfriendly" content="true" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="description" content="Mordenize" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="Mordenize" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <!-- --------------------------------------------------- -->
+    <!-- Favicon -->
+    <!-- --------------------------------------------------- -->
+    <link rel="shortcut icon" type="image/png"
+        href="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" />
+
+
+    <!-- --------------------------------------------------- -->
+    <!-- Core Css -->
+    <!-- --------------------------------------------------- -->
+    <link id="themeColors" rel="stylesheet" href="../../dist/css/style.min.css" />
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader">
+        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico"
+            alt="loader" class="lds-ripple img-fluid" />
+    </div>
+    <!-- --------------------------------------------------- -->
+    <!-- Body Wrapper -->
+    <!-- --------------------------------------------------- -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- --------------------------------------------------- -->
+        <!-- Sidebar -->
+        @include('layouts.components.sidebar');
+        <!-- --------------------------------------------------- -->
+
+
+        <!-- --------------------------------------------------- -->
+        <!-- Main Wrapper -->
+        <!-- --------------------------------------------------- -->
+        <div class="body-wrapper">
+            <!-- --------------------------------------------------- -->
+            <!-- Header Start -->
+            <!-- --------------------------------------------------- -->
+            @include('layouts.components.navbar')
+            <!-- --------------------------------------------------- -->
+            <!-- Header End -->
+            <!-- --------------------------------------------------- -->
+            <div class="container-fluid">
+                @yield('content')
+            </div>
         </div>
-    </body>
+    </div>
+
+
+    <!-- ---------------------------------------------- -->
+    <!-- Customizer -->
+    <!-- ---------------------------------------------- -->
+
+    <!-- ---------------------------------------------- -->
+    <!-- Import Js Files -->
+    <!-- ---------------------------------------------- -->
+    <script src="{{ asset('dist/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('dist/libs/simplebar/dist/simplebar.min.js') }}"></script>
+    <script src="{{ asset('dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- ---------------------------------------------- -->
+    <!-- core files -->
+    <!-- ---------------------------------------------- -->
+    <script src="{{ asset('dist/js/app.min.js') }}"></script>
+    <script src="{{ asset('dist/js/app.init.js') }}"></script>
+    <script src="{{ asset('dist/js/app-style-switcher.js') }}"></script>
+    <script src="{{ asset('dist/js/sidebarmenu.js') }}"></script>
+
+    <script src="{{ asset('dist/js/custom.js') }}"></script>
+
+    <!-- ---------------------------------------------- -->
+    <!-- current page js files -->
+    <!-- ---------------------------------------------- -->
+    <script src="{{ asset('dist/js/apps/jquery.PrintArea.js') }}"></script>
+    <script src="{{ asset('dist/js/apps/invoice.js') }}"></script>
+</body>
+
+
+<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/app-invoice.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 01:59:15 GMT -->
+
 </html>
