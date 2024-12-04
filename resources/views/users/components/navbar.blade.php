bc78ded573dbd70ad435070efd26091f2cc53caf
FrontierMar <rozakadm@gmail.com>
2024-12-04 16:04:00 +0700

    main

--- resources/views/users/components/navbar.blade.php
+++ resources/views/users/components/navbar.blade.php
@@ -36,20 +36,20 @@
     <!-- Modal -->
     <!-- Modal dengan backdrop yang dinonaktifkan -->
     <div class="modal fade mx-l" id="postModal" tabindex="-1" aria-labelledby="postModalLabel" aria-hidden="true"
-    data-bs-backdrop="false">
-    <div class="modal-dialog modal-lg"> <!-- Tambahkan kelas modal-lg atau modal-xl -->
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="postModalLabel">Create Post</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- Form to create post -->
-                @include('posts.create')
+        data-bs-backdrop="false">
+        <div class="modal-dialog modal-lg"> <!-- Tambahkan kelas modal-lg atau modal-xl -->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="postModalLabel">Create Post</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form to create post -->
+                    @include('posts.create')
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
