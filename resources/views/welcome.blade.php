9258b46aeea7a7f36b9b7c93dc00d10df4dee3f1
FrontierMar <rozakadm@gmail.com>
2025-02-07 22:15:58 +0700

    Rebuild Project 2024 |

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -165,7 +165,7 @@
                             </div>
                         </div>
                     </main>
-
+                    
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
