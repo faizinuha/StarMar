ae144f5cf0c07fb5ff3b23c13ce76a576c01666e
TZ <rozakadm@gmail.com>
2025-03-17 19:45:48 +0700

    Menambahkan file besar dengan Git LFS

--- resources/views/livewire/follows.blade.php
+++ resources/views/livewire/follows.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button wire:click="toggleFollow"
-        class="{{ $isFollowing ? 'bg-red-500 text-white' : 'bg-green-500 text-white' }} px-3 py-2 " style="border-radius:25px">
+        class="{{ $isFollowing ? 'bg-red-500' : 'bg-green-400' }} px-3 py-2 " style="border-radius:25px">
         {{ $isFollowing ? 'Unfollow' : 'Follow' }}
     </button>
 </div>
--- resources/views/users/app.blade.php
+++ resources/views/users/app.blade.php
@@ -15,8 +15,8 @@
     <link rel="stylesheet" href="{{ asset('dist2/css/emoji.css') }}">
     <link rel="stylesheet" href="{{ asset('dist2/css/lightbox.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
 
     {{-- owl-coursel --}}
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
